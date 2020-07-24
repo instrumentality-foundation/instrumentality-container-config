57a5cee7adef2a08504ba5cbad5f2225208b34b8
Instrumentality Foundation <instrumentality-foundation@outlook.com>
2020-07-25 00:14:14 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Instrumentality examples
+
+It contains: docker-compose file, genesis block, docker config for iroha, 
+ed25519/sha3 keys compatible with iroha, example of private configs for python API
+
+**DO NOT USE THOSE VALUES IN PRODUCTION!!!**
---
+++ private/constants.py
@@ -0,0 +1,10 @@
+"""
+Defines private constants related to credentials, signing, etc.
+"""
+
+from iroha import Iroha
+
+public_key = "3fdaadfdfd373b670e9f08c62b082d96c0338a6d68f3927e772a26f2c9793d0b"
+private_key = "758938673f95101bfa2a0360bac83a1fc22d690ef87c45ee7f7f4311189af7bd"
+iroha_network = "rondo:50051"
+iroha_client = Iroha("alexandru_balan@Instrumentality")
